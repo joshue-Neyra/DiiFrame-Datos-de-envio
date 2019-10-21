bcc9eadbf656596f6b78c74d5c017532bbd1a919
JahirBenitez <JahirBenitez>
2019-10-21 00:33:49 -0400

    Elegir tamaño p1

--- build/digitalphoto/index.php
+++ build/digitalphoto/index.php
@@ -13,60 +13,8 @@
 <body id="page-top">
 
 	<!-- Navigation -->
-	<nav class="navbar navbar-expand-lg navbar-dark bg-white  text-dark" id="mainNav">
-		<div class="col-sm-4 visible-sm-block visible-md-block align-left">
-
-			<div class="collapse navbar-collapse navbar-brand text-dark" id="navbarResponsive2">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<a class="nav-link js-scroll-trigger text-dark" href="#download">Comprar</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link js-scroll-trigger text-dark" href="#howitworks">Como Funciona</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link js-scroll-trigger text-dark" href="#contact">Servicios / Productos</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-		<div class="col-sm-4 visible-sm-block visible-md-block">
-			<center><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive2" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-
-					<i class="fas fa-bars"></i>
-				</button><img src="/assets/img/logo.png" alt="Logo" style="width:200px;"><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-
-					<i class="fas fa-bars"></i>
-				</button></center>
-		</div>
-		<div class="col-sm-4 visible-sm-block visible-md-block">
-
-			<div class="collapse navbar-collapse" id="navbarResponsive">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<a class="nav-link js-scroll-trigger text-warning" href="#download">
-							<p><i class="far fa-user"></i></p>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link js-scroll-trigger text-warning" href="#download">
-							<p><i class="fas fa-shopping-bag"></i></p>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link js-scroll-trigger text-warning" href="#features">
-							<p><i class="fas fa-comment-alt "></i></p>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a href="/build/"><button class="btn btn-warning text-dark">Iniciar Enmarcado</button></a>
-					</li>
-				</ul>
-			</div>
-		</div>
-		<div class="container">
-		</div>
-	</nav>
+	<!-- Navigation -->
+	<?php require $_SERVER['DOCUMENT_ROOT'].'/assets/components/principal/nav2.html'; ?>
 
 	<div class="row container align-self-center">
 		<div class="col-sm-2 bg-dark align-self-center">
@@ -89,7 +37,7 @@
 							<a id="preview"><img class="card-img-top" id="target" src="/assets/img/upload.jpg" alt="build"></a>
 							<div class="card-body container">
 								<div class="custom-file mb-3">
-									<input type="file" class="custom-file-input" id="imagen" value="Buscar">
+									<input type="file" class="custom-file-input" id="imagen" value="Buscar" accept="image/*">
 									<label class="custom-file-label" for="imagen">Buscar desde Ordenador</label>
 								</div>
 
@@ -153,7 +101,7 @@
 				
 				formData.append("croppedImage", blob,fileName);
 				$.ajax({
-					url: "upload.php",
+					url: "/assets/tools/imageupload/upload.php",
 					method: "POST",
 					data: formData,
 					processData: false,
