7be6be3c01715d4230b94546b982a33cde72046c
Bani Azarael <banimejia@codequark.com>
2019-11-19 15:41:01 -0600

    Agregar carrito JSON

--- assets/tools/Carrito/AgregarCarrito.php
+++ assets/tools/Carrito/AgregarCarrito.php
@@ -1,8 +1,25 @@
 <?php
+include 'Carrito.php';
+
 session_start();
-    $_SESSION['Producto_ID'][]=$_POST['Producto'];
-    $_SESSION['Tamano'][]=$_POST['Tamano'];
-    $_SESSION['Cantidad'][]=$_POST['Cantidad'];
-    $_SESSION['Precio'][]=$_POST['Precio'];
-    $_SESSION['Imagen'][]=$_POST['Imagen'];
-    echo count($_SESSION['Producto_ID']);
+
+$producto = $_POST['Producto'];
+$tamano = $_POST['Tamano'];
+$cantidad = $_POST['Cantidad'];
+$precio = $_POST['Precio'];
+$imagen = $_POST['Imagen'];
+
+$carrito = new Carrito();
+$carrito->setProducto($producto);
+$carrito->setTamano($tamano);
+$carrito->setCantidad($cantidad);
+$carrito->setPrecio($precio);
+$carrito->setImagen($imagen);
+
+$_SESSION['Producto_ID'][] = $producto;
+$_SESSION['Tamano'][] = $tamano;
+$_SESSION['Cantidad'][] = $cantidad;
+$_SESSION['Precio'][] = $precio;
+$_SESSION['Imagen'][] = $imagen;
+
+echo json_encode($carrito);
---
+++ assets/tools/Carrito/Carrito.php
@@ -0,0 +1,59 @@
+<?php
+
+class Carrito implements JsonSerializable {
+    private $producto;
+    private $tamano;
+    private $cantidad;
+    private $precio;
+    private $imagen;
+
+    public function setProducto($producto) {
+        $this->producto = $producto;
+    }
+
+    public function getProducto() {
+        return $this->producto;
+    }
+
+    public function setTamano($tamano) {
+        $this->tamano = $tamano;
+    }
+
+    public function getTamano() {
+        return $this->tamano;
+    }
+
+    public function setCantidad($cantidad) {
+        $this->cantidad = $cantidad;
+    }
+
+    public function getCantidad() {
+        return $this->cantidad;
+    }
+
+    public function setPrecio($precio) {
+        $this->precio = $precio;
+    }
+
+    public function getPrecio() {
+        return $this->precio;
+    }
+
+    public function setImagen($imagen) {
+        $this->imagen = $imagen;
+    }
+
+    public function getImagen() {
+        return $this->imagen;
+    }
+
+    public function jsonSerialize() {
+        return [
+            'Producto' => $this->getProducto(),
+            'Tamano' => $this->getTamano(),
+            'Cantidad' => $this->getCantidad(),
+            'Precio' => $this->getPrecio(),
+            'Imagen' => $this->getImagen()
+        ];
+    }
+}
