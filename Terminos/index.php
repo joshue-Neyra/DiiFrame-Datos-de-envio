21393f4e0c7bee596cfbe6067863846cf2d12cde
JahirBenitez <JahirBenitez>
2020-02-24 11:18:58 -0500

    Terminos y usos diana

--- Terminos/index.php
+++ Terminos/index.php
@@ -29,41 +29,128 @@
     <div class="container card my-5 mb-5">
         <H1>Términos y Condiciones de Uso</H1>
 
-        <p>Es requisito necesario para la adquisición de los productos que se ofrecen en este sitio, que lea y acepte los siguientes Términos y Condiciones que a continuación se redactan. El uso de nuestros servicios así como la compra de nuestros productos implicará que usted ha leído y aceptado los Términos y Condiciones de Uso en el presente documento. Todas los productos que son ofrecidos por nuestro sitio web pudieran ser creadas, cobradas, enviadas o presentadas por una página web tercera y en tal caso estarían sujetas a sus propios Términos y Condiciones. En algunos casos, para adquirir un producto, será necesario el registro por parte del usuario, con ingreso de datos personales fidedignos y definición de una contraseña.. </p>
+        <p>Dii Frame proveerá al usuario sus servicios de acuerdo a los siguientes Términos y Condiciones de
+            diiframe.com las cuales podrían ser actualizados o modificados por Dii Frame con o sin notificación
+            previa. Los sitios web diiframe.com, diiframe.mx están a disposición del usuario con la condición
+            que el usuario acepte sin objeción todos y cada uno de los Términos y Condiciones aquí descritos.</p>
 
-        <p>El usuario puede elegir y cambiar la clave para su acceso de administración de la cuenta en cualquier momento, en caso de que se haya registrado y que sea necesario para la compra de alguno de nuestros productos. https://diiframe.com.mx/ no asume la responsabilidad en caso de que entregue dicha clave a terceros.</p>
 
-        <p>Todas las compras y transacciones que se lleven a cabo por medio de este sitio web, están sujetas a un proceso de confirmación y verificación, el cual podría incluir la verificación del stock y disponibilidad de producto, validación de la forma de pago, validación de la factura (en caso de existir) y el cumplimiento de las condiciones requeridas por el medio de pago seleccionado. En algunos casos puede que se requiera una verificación por medio de correo electrónico.</p>
+        <H2>Descripción del servicio:</H2>
 
-        <p>Los precios de los productos ofrecidos en esta Tienda Online es válido solamente en las compras realizadas en este sitio web.</p>
+        <p>Dii Frame provee a los usuarios el acceso a varias herramientas en línea vinculadas a la impresión y
+            enmarcado de fotografías, enmarcado de arte y servicios de compra, al menos que se indique
+            expresamente lo contrario, cualquier nueva funcionalidad que mejore el servicio de Dii Frame
+            estará sujeto a éstos Términos y Condiciones.</p>
 
-        <H2>LICENCIA</H2>
+        <H2>Usuario:</H2>
 
-        <p>Diiframe S.A. DE C.V. a través de su sitio web concede una licencia para que los usuarios utilicen los productos que son vendidos en este sitio web de acuerdo a los Términos y Condiciones que se describen en este documento</p>
+        <p>El usuario puede ingresar y navegar en la página web o puede ingresar y registrarse con Dii Frame
+            y crear una cuenta. No es necesario que el usuario tenga una cuenta con Dii Frame para poder
+            realizar órdenes de nuestros productos, sin embargo deberá proveer a Dii Frame información
+            verdadera, correcta, actual y completa acerca de su persona para el registro de la cuenta o para la
+            solicitud de órdenes. . Si Dii Frame tiene bases razonables para sospechar que dicha información
+            es falsa, inexacta, desactualizada o incompleta, Dii Frame tendrá el derecho de suspender o cerrar
+            su cuenta y negarle el uso presente o futuro del servicio.</p>
+        <p>El usuario deberá proveer su nombre y correo electrónico y seleccionar una contraseña, misma
+            que no deberá transferir a un tercero. El usuario será responsable de mantener en
+            confidencialidad su contraseña, siendo también responsable único y absoluto por todas las
+            actividades que ocurran bajo su contraseña y nombre de usuario. El usuario se compromete a
+            notificar inmediatamente a Dii frame sobre cualquier uso no autorizado de su contraseña o
+            nombre de usuario o sobre cualquier otra falla de seguridad.</p>
+        <p>El usuario al registrarse en la página Web deberá proveer información verdadera, correcta, actual
+            y completa acerca de su persona según se requiere en el formulario de registro, y mantener y
+            actualizar en todo momento dichos datos de registro a fin de conservarlos veraces, correctos,
+            actuales y completos. Si el usuario suministra información que sea falsa, inexacta, desactualizada o
+            incompleta, o si Dii Frame tiene bases razonables para sospechar que dicha información es falsa,
+            inexacta, desactualizada o incompleta, Dii Frame tendrá el derecho de suspender o cerrar su
+            cuenta y negarle el uso presente o futuro del servicio.</p>
 
-        <H2>USO NO AUTORIZADO</H2>
+        <H2>Uso de marcas</H2>
 
-        <p>En caso de que aplique (para venta de software, templetes, u otro producto de diseño y programación) usted no puede colocar uno de nuestros productos, modificado o sin modificar, en un CD, sitio web o ningún otro medio y ofrecerlos para la redistribución o la reventa de ningún tipo.</p>
+        <p>El usuario acepta que todas las fotografías e imágenes que publica en nuestro sitio web le
+            pertenecen y que no hay sobre ellos derechos de exclusividad de terceros.</p>
+        <p>Las marcas, logotipos colocados en nuestro sitio web son propiedad de Dii Frame o de nuestros
+            otorgantes de licencias. Toda la información y las pantallas que aparecen en nuestro sitio web,
+            incluyendo materiales, documentos, servicios, diseño del sitio, texto, gráficos logos, imágenes e
+            íconos, así como la selección de lo antes mencionado son propiedad de Di Frame, al menos que se
+            especifique expresamente lo contrario.</p>
 
-        <H2>PROPIEDAD</H2>
+        <p>Todos los derechos no otorgados expresamente en este documento están reservados. Excepto
+            que lo exija la ley aplicable, cualquier reproducción, distribución, modificación, retransmisión o
+            publicación de cualquier material con derechos de autor está estrictamente prohibida sin el
+            consentimiento expreso por escrito del propietario de los derechos de autor.</p>
 
-        <p>Usted no puede declarar propiedad intelectual o exclusiva a ninguno de nuestros productos, modificado o sin modificar. Todos los productos son propiedad de los proveedores del contenido. En caso de que no se especifique lo contrario, nuestros productos se proporcionan sin ningún tipo de garantía, expresa o implícita. En ningún esta compañía será responsables de ningún daño incluyendo, pero no limitado a, daños directos, indirectos, especiales, fortuitos o consecuentes u otras pérdidas resultantes del uso o de la imposibilidad de utilizar nuestros productos.</p>
+        <p>El usuario comprende y reconoce que el software contiene información confidencial y que están
+            protegidos por derecho de autor por nuestra parte o por nuestros otorgantes de licencias
+            protegida bajo las leyes aplicables de propiedad intelectual y de otra naturaleza.</p>
+        <p>Cualquier material enviado por el usuario a Dii Frame, incluyendo, sin limitación, sus ilustraciones,
+            fotografías, imágenes, textos, gráficos y otros materiales están sujetos a los siguientes términos y
+            condiciones. El usuario conservará la propiedad de dichos materiales enviados y otorgará a Dii
+            frame y a nuestros diseñadores un derecho y una licencia limitados, no exclusivos, para usar,
+            reproducir, distribuir, crear trabajos derivados y mostrar públicamente dichos materiales enviados
+            por el usuario, para permitirle usar el servicio para crear, producir y comprar productos, en
+            relación con la producción o provisión de cualquier producto o servicio que solicite, para mostrarle
+            cómo aparecerían los materiales enviados por el usuario en nuestros productos o servicios, y para
+            permitir que Dii Frame que proporcione los Productos y Servicios.</p>
+        <p>Por ejemplo, cuando realiza un pedido de un producto, prepararemos, manipularemos (si es
+            necesario) y transmitiremos los materiales enviados por el usuario para el enmarcado, embalaje y
+            envío. Con respecto a los materiales enviados por un artista a Dii Frame, incluidos, sin limitación,
+            ilustraciones, fotografías, imágenes, texto, gráficos y otros materiales el artista conserva la
+            propiedad de todos los materiales enviados por este artista. Dii Frame acepta que trabajará
+            individualmente con un Artista para obtener cualquier derecho de uso, reproducción, distribución
+            y visualización pública de Materiales Enviados por el Artista en el Sitio o en otros materiales de
+            marketing de Dii Frame y únicamente con el consentimiento del Artista.</p>
 
-        <H2>POLÍTICA DE REEMBOLSO Y GARANTÍA</H2>
+        <h2>Reglas de información y Contenido</h2>
+        <p>Cuando el usuario accede al Sitio y / o los Servicios, obtiene acceso a varios tipos de imágenes,
+            video, audio, datos y otra información y materiales. El usuario acepta no revisar el contenido
+            publicado por otros y declara y garantiza que no utilizará ningún contenido de ninguna manera ni
+            enviará ningún Material enviado u otro Contenido que:</p>
+        <ul>
+            <li>a) Infrinja los derechos de autor, marca registrada, secreto comercial u otra propiedad
+                intelectual o derecho de propiedad de terceros;</li>
+            <li>b) Viola la privacidad, publicidad u otros derechos de terceros o cualquier otra ley, estatuto,
+                ordenanza o regulación;</li>
+            <li>c) Es falso o inexacto o se vuelve falso o inexacto en cualquier momento; Es discriminatorio,
+                ilegal, ilícito, obsceno, fraudulento, difamatorio, dañino, amenazante, pornográfico,
+                indecente, vulgar, acosador, descortés, odioso, abusivo o racial, étnico, religioso, sexual o
+                de cualquier otra forma, según lo determinemos en nuestra única discreción. ;</li>
+            <li>d) Revela o proporciona información protegida por cualquier ley, acuerdo o relación
+                fiduciaria, que incluye, entre otros, información confidencial o de propiedad de terceros;</li>
+            <li>e) Tergiversa tu identidad de cualquier manera;</li>
+            <li>f) Contiene virus, troyanos, spyware. malware, gusanos, bombas de tiempo, botes de
+                cancelación u otros dispositivos de desactivación u otros componentes dañinos destinados
+                a dañar, interferir de manera perjudicial, interceptar o expropiar de forma secreta
+                cualquier sistema, datos o información personal;</li>
+            <li>g) Promueve o alienta cualquier actividad ilegal; o</li>
+            <li>h) Tiene el potencial de crear responsabilidad para nosotros o hacer que violemos los
+                requisitos de o perdamos los servicios, en su totalidad o en parte, de nuestros
+                proveedores de servicios de Internet u otros proveedores.</li>
+        </ul>
+        <p>Dii Frame tiene el derecho, pero no la obligación, a su plena discreción de rechazar o remover
+            cualquier contenido publicado, por el usuario que considere inadecuado, ya sea en virtud de un
+            requerimiento judicial o administrativo, o a su solo entendimiento.</p>
 
-        <p>En el caso de productos que sean mercancías irrevocables no-tangibles, no realizamos reembolsos después de que se envíe el producto, usted tiene la responsabilidad de entender antes de comprarlo. Le pedimos que lea cuidadosamente antes de comprarlo. Hacemos solamente excepciones con esta regla cuando la descripción no se ajusta al producto. Hay algunos productos que pudieran tener garantía y posibilidad de reembolso pero este será especificado al comprar el producto. En tales casos la garantía solo cubrirá fallas de fábrica y sólo se hará efectiva cuando el producto se haya usado correctamente. La garantía no cubre averías o daños ocasionados por uso indebido. Los términos de la garantía están asociados a fallas de fabricación y funcionamiento en condiciones normales de los productos y sólo se harán efectivos estos términos si el equipo ha sido usado correctamente. Esto incluye:</p>
-        <li>De acuerdo a las especificaciones técnicas indicadas para cada producto.</li>
-        <li> En condiciones ambientales acorde con las especificaciones indicadas por el fabricante.</li>
-        <li> En uso específico para la función con que fue diseñado de fábrica.</li>
-        <li> En condiciones de operación eléctricas acorde con las especificaciones y tolerancias indicadas.</li>
-        
-        <h2>COMPROBACIÓN ANTIFRAUDE</h2>
-        <p>La compra del cliente puede ser aplazada para la comprobación antifraude. También puede ser suspendida por más tiempo para una investigación más rigurosa, para evitar transacciones fraudulentas.</p>
-        
-        <h2>PRIVACIDAD</h2>
-        <p>Este sitio web https://diiframe.com.mx/ garantiza que la información personal que usted envía cuenta con la seguridad necesaria. Los datos ingresados por usuario o en el caso de requerir una validación de los pedidos no serán entregados a terceros, salvo que deba ser revelada en cumplimiento a una orden judicial o requerimientos legales.</p>
-        
-        <p>La suscripción a boletines de correos electrónicos publicitarios es voluntaria y podría ser seleccionada al momento de crear su cuenta.</p>
+        <h2>Precios y Productos</h2>
+
+        <p>Nos esforzamos por mostrar nuestros productos y sus colores con la mayor precisión posible.
+            Dicho esto, los colores mostrados de los productos dependerán de su monitor y no podemos
+            garantizar que su monitor representará con precisión los colores reales de los productos.</p>
+        <p>Los precios están sujetos a cambios. No podemos confirmar el precio de un artículo hasta que
+            realice un pedido. Además, en el caso de que recibamos sus ilustraciones y determinemos que el
+            producto que ha pedido no funcionará para sus ilustraciones, nos comunicaremos con usted para
+            determinar si desea cambiar su orden a un producto diferente. En caso de que cambie su orden, el
+            precio de su orden probablemente cambiará.</p>
+        <p>Usted nos autoriza a cobrar su método de pago por cualquier cambio en su pedido que haya
+            aprobado. Los precios ya incluyen IVA.</p>
+        <p>El resultado del pedido es consecuencia de su elección por lo que cualquier cambio podría originar
+            una variación en precio.</p>
+        <p>El éxito de la impresión dependerá del archivo (condiciones, formato, pixeles y calidad de la
+            imagen).</p>
+
+        <p>Al confirmar el pedido no hay cambios ni devoluciones.
+            Dii Frame no se hace responsable por contingencias climatológicas, políticas o sociales que
+            conlleven a retrasos en nuestras entregas..</p>
         <p>Diiframe S.A. DE C.V. reserva los derechos de cambiar o de modificar estos términos sin previo aviso.</p>
         <p class="text-muted">Ultima modificación: 18 de Febrero, 2020</p>
     </div>
