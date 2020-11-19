3be11eee1b7098d86c7ea8f2bdd3e54aa49ec452
Jahir Benitez <jahirbenitez@codequark.com>
2020-11-18 19:47:58 -0500

    update instagram

--- assets/js/Instagram/instagram.js
+++ assets/js/Instagram/instagram.js
@@ -6,20 +6,13 @@ $(document).ready(function () {
 function InstagramGetMedia() {
 
     var at = document.getElementById("ig_access_token").value;
-    console.log(at)
-    console.log(at.toString)
-    alert(at);
     $.ajax({
         "url": 'https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type&access_token=' + at,
         "method": "GET",
         "dataType": "jsonp",
         "cache": false,
         "success": function (response) {
-            console.log(response);
-            var algo = response.data;
-            
-            //console.log(algo[0].media_url);
-            console.log(response.toString);
+            var algo = response.data;         
             for (i = 0; i < algo.length; i++) {
                 if (algo[i].media_type != "VIDEO") {
                     var url = "'" + algo[i].media_url.toString() + "'";
